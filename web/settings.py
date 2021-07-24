5eda20142ca4b25cd8118a25b0f48d8256c998a3
ThumbsUpMike <michalbujalski22@gmail.com>
2021-07-24 01:04:50 +0100

    33

--- web/settings.py
+++ web/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6ziui5y(!n^lm!#$z$p0#-6ort9^y%41qv5z+_mecousu(4g*k'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    contact = Contact.objects.all()
+    context = {
+        "contact": contact
+    }
+    return render(request, 'index.html', context)
