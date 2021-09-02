6156e4838d82784b2c64cfb95989248cdcaaacbf
ThumbsUpMike <michalbujalski22@gmail.com>
2021-09-02 22:37:47 +0100

    Start with contact response

--- web/urls.py
+++ web/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('webapp/', include('webapp.urls', namespace='webapp')),
+    path('', include('webapp.urls', namespace='webapp')),
  
 
 ]
