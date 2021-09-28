074979373b9fec1e6fd93893354ff6633eb0b404
ThumbsUpMike <michalbujalski22@gmail.com>
2021-09-28 10:20:34 +0100

    SetUp Test.py

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from webapp.models import Contact
 
 # Create your tests here.
+
+class ContactTestCase(TestCase):
+    def setUp(self):
+        Contact.objects.create(
+            first_name= "Rob",
+            last_name= "Seek",
+            email= "robseek@email.com",
+            reason= "Work",
+            my_response= True,
+            body= "some text",
+        )
+
+    def test_contacts_email(self):
+        rob = Contact.objects.get(first_name= "Rob")
+        self.assertEqual(rob.first_name, "Rob")
