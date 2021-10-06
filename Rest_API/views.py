7ecb328773ae393b7742506f9c0144fd41e00a63
ThumbsUpMike <michalbujalski22@gmail.com>
2021-10-06 20:12:54 +0100

    rest

---
+++ Rest_API/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+
+from rest_framework.views import APIView
+from rest_framework import routers 
+from rest_framework.urlpatterns import format_suffix_patterns
+# Create your views here.
--- web/settings.py
+++ web/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
